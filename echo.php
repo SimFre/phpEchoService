d0f850762fd71d05991a3278a9fd85fb76bc1ef5
Simon Fredriksson <simon@lan2k.org>
2022-11-01 12:37:46 +0000

    Output to file

--- echo.php
+++ echo.php
@@ -11,14 +11,14 @@ $data->files = $_FILES;
 $data->server = $_SERVER;
 $data->headers = getallheaders();
 $data->input = file_get_contents("php://input");
-$data->contentMode = null;
-$data->contentType = null;
+$data->contentExtension = "dat";
+$data->contentType = "application/octet-stream";
 
 $p = $_SERVER['PATH_INFO'] ?? "unknown";
 $output = "";
 switch($p) {
     case "/xml":
-        $data->contentMode = "xml";
+        $data->contentExtension = "xml";
         $data->contentType = "text/xml";
         $xml = '<root>';
         $xml .= generate_xml_from_array($data, "root", 0);
@@ -31,14 +31,14 @@ switch($p) {
         break;
 
     case "/plain":
-        $data->contentMode = "plain";
+        $data->contentExtension = "txt";
         $data->contentType = "text/plain";
         $output = print_r($data, true);
         break;
 
     case "/json":
     default:
-        $data->contentMode = "json";
+        $data->contentExtension = "json";
         $data->contentType = "application/json";
         $output = json_encode($data, JSON_PRETTY_PRINT);
         break;
@@ -70,3 +70,7 @@ function generate_xml_from_array($array, $node_name, $depth = 0)
 header("Content-type: " . $data->contentType);
 echo $output . PHP_EOL;
 error_log($output . PHP_EOL);
+
+$ymd = date("YmdHis");
+@mkdir("output");
+@file_put_contents("output/echo_".$ymd.".".$data->contentExtension, $output);
